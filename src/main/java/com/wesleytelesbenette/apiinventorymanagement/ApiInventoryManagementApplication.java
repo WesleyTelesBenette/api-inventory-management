12db3666238944ce57def9624562f66c6484ad43
Wesley T. Benette <weluonf@gmail.com>
2024-02-02 22:59:25 -0300

    Configurações do banco de dados

--- src/main/java/com/wesleytelesbenette/apiinventorymanagement/ApiInventoryManagementApplication.java
+++ src/main/java/com/wesleytelesbenette/apiinventorymanagement/ApiInventoryManagementApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiInventoryManagementApplication {
-
-	public static void main(String[] args) {
+public class ApiInventoryManagementApplication
+{
+	public static void main(String[] args)
+	{
 		SpringApplication.run(ApiInventoryManagementApplication.class, args);
 	}
 
