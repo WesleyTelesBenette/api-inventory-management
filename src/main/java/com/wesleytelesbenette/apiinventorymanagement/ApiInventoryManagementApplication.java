c18b84e001413dd88c490f8388326e435c08a04c
Wesley T. Benette <weluonf@gmail.com>
2024-02-02 22:14:56 -0300

    Criação do Projeto :)

---
+++ src/main/java/com/wesleytelesbenette/apiinventorymanagement/ApiInventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.wesleytelesbenette.apiinventorymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInventoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInventoryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wesleytelesbenette/apiinventorymanagement/ApiInventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesleytelesbenette.apiinventorymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
